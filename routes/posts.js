873b7578176ac9806bdddcbc88554f77a130fc39
JohnPerson05 <jpnarral@gmail.com>
2022-05-05 13:34:35 +0800

    changing the to this in delete post response

--- routes/posts.js
+++ routes/posts.js
@@ -34,7 +34,7 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.deleteOne();
-      res.status(200).json("The post has been deleted");
+      res.status(200).json("this post has been deleted");
     } else {
       res.status(403).json("You can delete only your post");
     }
